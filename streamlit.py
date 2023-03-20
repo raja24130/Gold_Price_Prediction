71000324fa036b3205a1d847000758f802066272
raja24130 <117657790+raja24130@users.noreply.github.com>
2023-03-20 11:26:39 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,35 +1,55 @@
+import pickle
 import streamlit as st
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+import requests
 
-# Load and preprocess the data
-data = pd.read_csv("gold_prices.csv")
-data["Date"] = pd.to_datetime(data["Date"])
-data.set_index("Date", inplace=True)
-X = data.index.values.astype(float).reshape(-1, 1)
-y = data["Close"].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
 
-# Train the model
-model = LinearRegression()
-model.fit(X_train, y_train)
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
 
-# Make predictions
-y_pred = model.predict(X_test)
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+    return recommended_movie_names,recommended_movie_posters
 
-# Create a Streamlit app
-st.title("Gold Price Prediction App")
-st.write("Enter a date to get the predicted gold price.")
-date_input = st.date_input("Date", value=pd.to_datetime("2022-01-01"), min_value=data.index.min(), max_value=data.index.max())
-date_value = date_input.timestamp()
-price = model.predict([[date_value]])
-st.write("Predicted Gold Price:", round(price[0], 2))
 
-# Display model performance metrics
-st.write("Model Performance:")
-st.write("Mean Squared Error:", round(mse, 2))
-st.write("R-squared:", round(r2, 2))
+st.header('Movie Recommender System')
+movies = pickle.load(open('model/movie_list.pkl','rb'))
+similarity = pickle.load(open('model/similarity.pkl','rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.beta_columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
